2b2df2d35882a5892811a9af5b623b60c4ee9450
Adriana Oviedo <oviedolopezadriana13@gmail.com>
2022-03-23 22:29:49 -0300

    add_graph

--- public/covid19/assets/scripts.js
+++ public/covid19/assets/scripts.js
@@ -1,57 +1,86 @@
 // getData
 const getData = async () => {
-    try {
-      const response = await fetch('http://localhost:3000/api/total', {
-        method: 'GET',
-        headers: {
-          //Authorization: `Bearer ${jwt}`,
-        },
-      })
-      const { data } = await response.json()
-      return data
-    } catch (err) {
-      console.error(`Error: ${err}`)
-    }
+  try {
+    const response = await fetch('http://localhost:3000/api/total', {
+      method: 'GET',
+      headers: {
+        //Authorization: `Bearer ${jwt}`,
+      },
+    })
+    const { data } = await response.json()
+    return data
+  } catch (err) {
+    console.error(`Error: ${err}`)
   }
+}
 // Informacion de la API
-(async()=>{
-const data = await getData()
-const moreThanTenThousand = data.filter(cases=>cases.confirmed>10000)
-console.log(moreThanTenThousand)
-})()
-// Gráfico
-const ctx = document.getElementById('myChart').getContext('2d');
+;(async () => {
+  const data = await getData()
+  const moreThanTenThousand = data.filter((cases) => cases.confirmed > 2000000)
+  console.log(moreThanTenThousand)
+
+ 
+  //-----------
+  const graph = moreThanTenThousand.map((location) => {
+    const label = location.location
+    return label
+    
+  })
+  const confirmed = moreThanTenThousand.map(p => p.confirmed)
+  const deaths = moreThanTenThousand.map(p => p.deaths)
+  console.log(deaths)
+  const recovered = moreThanTenThousand.map(p => p.recovered)
+  const active = moreThanTenThousand.map(p => p.active)
+  
+  // Gráfico
+  const data2 = {
+    labels: graph,
+    datasets: [
+      {
+        label: 'Casos Activos',
+        data: confirmed,
+        borderColor: 'red',
+        backgroundColor: 'red',
+      },
+      {
+        label: 'Muertos',
+        data: deaths,
+        borderColor: 'blue',
+        backgroundColor: 'blue',
+      },
+      {
+        label: 'Recuperados',
+        data: recovered,
+        borderColor: 'yellow',
+        backgroundColor: 'yellow',
+      },
+      {
+        label: 'Activos',
+        data: active,
+        borderColor: 'green',
+        backgroundColor: 'green',
+      }
+
+    ]
+  };
+const ctx = document.getElementById('myChart').getContext('2d')
 const myChart = new Chart(ctx, {
     type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
+    data: data2,
     options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Chart.js Bar Chart'
         }
-    }
-});
+      }
+    },
+})
+
+  console.log (graph)
+  //------------------
+})()
